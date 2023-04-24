a56322f92e8b59914b412c98018d93148218372b
Ukamaka95 <mirabelukaegbu19@gmail>
2023-04-24 15:35:50 -0700

    I sometimes suffer from insomnia.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,15 @@
-#include <stdlib.h>	
-#include <time.h>
 #include <stdio.h>
-/**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
-*/
+*
 int main(void)
+
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char c = 'a';
 
-	for (i = 0; i < 26; i++)
+	while (c <= 'z')
 	{
-	putchar(alp[i]);
+	putchar(c);
+	c++;
+	}
 	putchar('\n');
-	return (0);
+	(return 0;)
 }
-

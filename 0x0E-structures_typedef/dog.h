857c64c2278fc17fd85b32985cb58c2540d1fbda
Ukamaka95 <mirabelukaegbu19@gmail>
2023-05-22 15:49:15 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,29 @@
 #ifndef DOG_H
 #define DOG_H
+
+
 /**
- * struct dog - a dog's basic information
- * @name: first member
- * @age:second member
- * @owner: third name
- *
- * Description: longer description
- */
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description: Longer description
+*/
 struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+/**
+* dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
@@ -21,4 +31,6 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
+
 #endif
+
